6643c0fbc22e2828829f43d387e078877c9ef08d
tfutada <tafutada@gmail.com>
2024-03-23 23:28:22 +0900

    dispatcher

---
+++ src/main/kotlin/jp/tf/coroutine3/Confine.kt
@@ -0,0 +1,20 @@
+package jp.tf.coroutine3
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    launch(Dispatchers.Unconfined) { // not confined -- will work with main thread
+        println("Unconfined      : I'm working in thread ${Thread.currentThread().name}")
+        delay(500)
+        println("Unconfined      : After delay in thread ${Thread.currentThread().name}")
+    }
+    launch { // context of the parent, main runBlocking coroutine
+        println("main runBlocking: I'm working in thread ${Thread.currentThread().name}")
+        delay(1000)
+        println("main runBlocking: After delay in thread ${Thread.currentThread().name}")
+    }
+
+    println("Hello, World!")
+}
+
