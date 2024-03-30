4bd94b74f63fd3bfc9c3611bb71e5b07a2303d0f
tfutada <tafutada@gmail.com>
2024-03-30 14:36:03 +0900

    aws sdk s3

---
+++ src/main/kotlin/jp/tf/aws/putobj/Upload.kt
@@ -0,0 +1,40 @@
+package jp.tf.jp.tf.aws.putobj
+
+import aws.sdk.kotlin.runtime.auth.credentials.ProfileCredentialsProvider
+import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.s3.model.PutObjectRequest
+import aws.smithy.kotlin.runtime.content.asByteStream
+import kotlinx.coroutines.runBlocking
+import java.io.File
+
+fun main() = runBlocking {
+    putObjects("futa-taka-bucket-1", "access-log-20241231.log")
+}
+
+// Define a higher-order function with a lambda receiver for S3Client operations
+suspend fun withS3Client(block: suspend S3Client.() -> Unit) {
+    S3Client {
+        region = "us-east-1"
+        credentialsProvider = ProfileCredentialsProvider("mfa")
+    }.use { s3Client ->
+        block(s3Client)
+    }
+}
+
+suspend fun putObjects(bucketName: String, objectPath: String) {
+    val metadataVal = mutableMapOf<String, String>()
+    metadataVal["myVal"] = "test123"
+
+    val request = PutObjectRequest {
+        bucket = bucketName
+        key = objectPath
+        metadata = metadataVal
+        body = File(objectPath).asByteStream()
+    }
+
+    withS3Client {
+        val response = putObject(request)
+        println("Tag information is ${response.eTag}")
+    }
+}
+
