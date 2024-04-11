ed9da942c557a496c587b8f16eb31e23aef30650
tfutada <tafutada@gmail.com>
2024-04-11 11:08:15 +0900

    Dispatchers

--- src/main/kotlin/jp/tf/coroutine1/ManyCoroutines.kt
+++ src/main/kotlin/jp/tf/coroutine1/ManyCoroutines.kt
@@ -3,12 +3,15 @@ package jp.tf.jp.tf.coroutine1
 import kotlinx.coroutines.*
 
 fun main() = runBlocking {
-    repeat(50_000) { // launch a lot of coroutines
+    repeat(1000) { // launch a lot of coroutines
         launch(Dispatchers.IO) {
-//            delay(5000L)
-            Thread.sleep(5000L)
+            delay(5000L)
+//            Thread.sleep(5000L)
             print(".")
         }
     }
-}
 
+    println("Press Enter to exit")
+    readln()
+    println()
+}
