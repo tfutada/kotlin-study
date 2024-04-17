9dc29711e94e0de5c3d19219cc27725ff1fa0858
tfutada <tafutada@gmail.com>
2024-04-17 17:35:51 +0900

    udp server with log rotation

--- src/main/kotlin/jp/tf/udp/serverrotation/UDPServer.kt
+++ src/main/kotlin/jp/tf/udp/serverrotation/UDPServer.kt
@@ -7,35 +7,42 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
 
-// 1. ログローテーション機能
-// 2. フォルダ・ウォッチ機能
-// 3. S3ファイル・アップロード機能
-//
-// 設定したファイルサイズになったら、ファイルをリネームして、新しいファイルを作成する。
-// 別スレッドで、フォルダを監視し、ファイルが追加(ログローテーションのタイミング)されたら、そのファイルをS3にアップロードする。
+const val LogFileNamePrefix = "logs/netflow-packet"
+const val LogFileName = "$LogFileNamePrefix.log"
+
 fun main() {
     val selectorManager = SelectorManager(Dispatchers.IO)
     val serverSocket = aSocket(selectorManager).udp().bind(InetSocketAddress("::", 5106))
     println("Server is listening at ${serverSocket.localAddress}")
 
+    val maxFileSize = 100_000  // Max file size in bytes
+    var logFile = File(LogFileName)
+
     CoroutineScope(Dispatchers.IO).launch {
         while (true) {
-            // Each packet processing is offloaded to a new coroutine.
-            val packet = serverSocket.receive()  // This is a suspending function and will not block the thread.
+            val packet = serverSocket.receive()
 
             launch {
-                val message = packet.packet.readBytes().decodeToString()
-                // CPU intensive task
-                val ret = withContext(Dispatchers.IO) {
-                    // write message to a file
-                    java.io.File("udp_server3.txt").appendText("$message\n")
-                    "Done"
+                // Decode the packet data
+                val udpPacket = packet.packet.readBytes().decodeToString()
+
+                // Log rotation check and execution
+                withContext(Dispatchers.IO) {
+                    if (logFile.length() > maxFileSize) {
+                        val newFileName = "${LogFileNamePrefix}.${System.currentTimeMillis()}.log"
+                        logFile.renameTo(File(newFileName))
+                        logFile = File(LogFileName)  // reset log file
+                    }
+
+                    // Write message to the log file
+                    logFile.appendText("$udpPacket\n")
                 }
 
-                println("Received from ${packet.address}: $message $ret")
+                println("Received from ${packet.address}: $udpPacket")
                 // optional: echo the message back
-                serverSocket.send(Datagram(ByteReadPacket("Echo->[ $message".encodeToByteArray()), packet.address))
+                serverSocket.send(Datagram(ByteReadPacket("Echo->[ $udpPacket".encodeToByteArray()), packet.address))
             }
         }
     }
