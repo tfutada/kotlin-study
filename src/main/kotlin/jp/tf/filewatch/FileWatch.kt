5a008e64e48c5bf5d2e3c6f4b827204086aebb46
tfutada <tafutada@gmail.com>
2024-04-17 22:24:51 +0900

    udp server with log rotation

---
+++ src/main/kotlin/jp/tf/filewatch/FileWatch.kt
@@ -0,0 +1,24 @@
+package jp.tf.filewatch
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+
+import java.nio.file.Paths
+
+fun main() = runBlocking {
+    val path = Paths.get("/Users/tafu/futasoft/kotlin/kotlin-study/logs")
+    val directoryWatcher = DirectoryWatcher(path)
+
+    directoryWatcher.onCreate {
+        println("New file added: $this")
+        // Additional logic can be added here, using `this` to refer to the Path instance
+    }
+
+    println("Watching directory. Press enter to exit...")
+    launch {
+        directoryWatcher.watchDirectoryEvents()
+    }
+    readLine()  // Keep the program running until user input
+
+    println("Exiting...")
+}
---
+++ src/main/kotlin/jp/tf/filewatch/Watcher.kt
@@ -0,0 +1,59 @@
+package jp.tf.filewatch
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import java.nio.file.*
+import java.nio.file.StandardWatchEventKinds.*
+import kotlin.io.path.name
+
+class DirectoryWatcher(private val path: Path) {
+    private val watcher = path.fileSystem.newWatchService()
+    var onCreate: Path.() -> Unit = {}  // Using a lambda receiver here
+
+    init {
+        path.register(watcher, ENTRY_CREATE)
+        // print the full path to watch
+        println("Watching: ${path.toAbsolutePath()}")
+    }
+
+    fun onCreate(action: Path.() -> Unit) {
+        onCreate = action
+    }
+
+    suspend fun watchDirectoryEvents() = coroutineScope {
+        val eventChannel = Channel<WatchEvent<*>>()
+        launch(Dispatchers.IO) {
+            try {
+                while (!eventChannel.isClosedForSend) {
+                    val key: WatchKey = watcher.take()
+                    key.pollEvents().forEach {
+                        eventChannel.send(it)
+                    }
+                    key.reset()
+                }
+            } catch (e: InterruptedException) {
+                eventChannel.close()
+            } finally {
+                watcher.close()
+            }
+        }
+
+        launch {
+            for (event in eventChannel) {
+                val kind = event.kind()
+                if (kind == OVERFLOW) {
+                    continue
+                }
+
+                @Suppress("UNCHECKED_CAST")
+                val ev = event as WatchEvent<Path>
+                val filename = ev.context()
+
+                when (kind) {
+                    ENTRY_CREATE -> filename.onCreate()  // Calling the lambda receiver
+                    else -> println("Event kind: $kind at $filename")
+                }
+            }
+        }
+    }
+}
