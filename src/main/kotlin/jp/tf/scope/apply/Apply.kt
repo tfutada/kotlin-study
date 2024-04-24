7457f38bdff15acdd3663a8ccf76f6124b8497aa
tfutada <tafutada@gmail.com>
2024-04-24 19:34:24 +0900

    fix

--- src/main/kotlin/jp/tf/scope/apply/Apply.kt
+++ src/main/kotlin/jp/tf/scope/apply/Apply.kt
@@ -4,9 +4,18 @@ fun main() {
     val message = StringBuilder().apply {
         append("Hello, ")
         append("world!")
+
     }
 
     println(message.toString())
+
+    ArrayList<String>().apply {
+        add("Hello")
+        add("World")
+        this.add("!")
+    }.also {
+        println(it)
+    }
 }
 
 
